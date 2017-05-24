65179ad4e3d9e7e53d4b9ee9a1b0e1dfa000e3fc
Ognen Ivanovski <ognen.ivanovski@netcetera.com>
2017-05-24 15:16:20 +0200

    twitter app credentials specified as package parameters

--- example/android/app/src/main/java/com/example/MainApplication.java
+++ example/android/app/src/main/java/com/example/MainApplication.java
@@ -7,12 +7,17 @@ import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.react.shell.MainReactPackage;
 import com.facebook.soloader.SoLoader;
-import com.netcetera.reactnative.twitterkit.RNTwitterKitPackage;
+import com.netcetera.reactnative.twitterkit.ReactTwitterKitPackage;
 
 import java.util.Arrays;
 import java.util.List;
 
 public class MainApplication extends Application implements ReactApplication {
+
+  // set your keys here!
+
+  private static final String CONSUMER_KEY = null;
+  private static final String CONSUMER_SECRET = null;
 
   private final ReactNativeHost mReactNativeHost = new ReactNativeHost(this) {
     @Override
@@ -24,7 +29,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
-          new RNTwitterKitPackage()
+          new ReactTwitterKitPackage(CONSUMER_KEY, CONSUMER_SECRET)
       );
     }
   };
@@ -38,8 +43,6 @@ public class MainApplication extends Application implements ReactApplication {
   public void onCreate() {
     super.onCreate();
     SoLoader.init(this, /* native exopackage */ false);
-    //initWithInflate fabric
-    RNTwitterKitPackage.initFabric(getApplicationContext());
   }
 
 }
